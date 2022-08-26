2ba440da0b33956a6fce1b2eb451e2e64b11e419
Slim Babay <xslim24x@gmail.com>
2022-08-26 17:03:22 -0500

    Updated v1.0 syllabics

---
+++ AboriginalSyllabics/Syllabics.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AboriginalSyllabics
+{
+    partial class Syllabics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSyl = new System.Windows.Forms.TextBox();
+            this.txtRoman = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.tablegroup = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtSyl);
+            this.groupBox1.Controls.Add(this.txtRoman);
+            this.groupBox1.Location = new System.Drawing.Point(12, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(875, 135);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtSyl
+            // 
+            this.txtSyl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSyl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSyl.Location = new System.Drawing.Point(13, 27);
+            this.txtSyl.Margin = new System.Windows.Forms.Padding(10);
+            this.txtSyl.Multiline = true;
+            this.txtSyl.Name = "txtSyl";
+            this.txtSyl.Size = new System.Drawing.Size(419, 95);
+            this.txtSyl.TabIndex = 5;
+            this.txtSyl.WordWrap = false;
+            // 
+            // txtRoman
+            // 
+            this.txtRoman.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRoman.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoman.Location = new System.Drawing.Point(439, 27);
+            this.txtRoman.Margin = new System.Windows.Forms.Padding(10);
+            this.txtRoman.Multiline = true;
+            this.txtRoman.Name = "txtRoman";
+            this.txtRoman.Size = new System.Drawing.Size(419, 95);
+            this.txtRoman.TabIndex = 4;
+            this.txtRoman.WordWrap = false;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(739, 22);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(131, 49);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // tablegroup
+            // 
+            this.tablegroup.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tablegroup.AutoSize = true;
+            this.tablegroup.ColumnCount = 5;
+            this.tablegroup.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tablegroup.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tablegroup.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tablegroup.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tablegroup.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tablegroup.Location = new System.Drawing.Point(13, 30);
+            this.tablegroup.Name = "tablegroup";
+            this.tablegroup.RowCount = 14;
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+            this.tablegroup.Size = new System.Drawing.Size(845, 907);
+            this.tablegroup.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.tablegroup);
+            this.groupBox2.Location = new System.Drawing.Point(12, 229);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(875, 955);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // Syllabics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(895, 1464);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Syllabics";
+            this.Text = "Syllabics";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtRoman;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.TextBox txtSyl;
+        private System.Windows.Forms.TableLayoutPanel tablegroup;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ AboriginalSyllabics/Syllabics.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboriginalSyllabics
+{
+    public partial class Syllabics : Form
+    {
+        string[] letters = { "" , "p", "t", "k","s","sh","ch","n","m","y","w","l","r"};
+        string[] vow = { "ay", "ee", "oo", "ah","" };
+        char[][] syl = {
+            new char[]{'ᐁ','ᐃ','ᐅ','ᐊ'},
+            new char[]{'ᐯ','ᐱ','ᐳ','ᐸ'},
+            new char[]{'ᑌ','ᑎ','ᑐ','ᑕ'},
+            new char[]{'ᑫ','ᑭ','ᑯ','ᑲ'},
+            new char[]{'ᓭ','ᓯ','ᓱ','ᓴ'},
+            new char[]{'ᔐ','ᔑ','ᔓ','ᔕ'},
+            new char[]{'ᒉ','ᒋ','ᒍ','ᒐ'},
+            new char[] {'ᓀ','ᓂ','ᓄ','ᓇ'},
+            new char[] {'ᒣ','ᒥ','ᒧ','ᒪ'},
+            new char[] {'ᔦ','ᔨ','ᔪ','ᔭ'},
+            new char[] {'ᐌ','ᐎ','ᐒ','ᐗ'},
+            new char[] {'ᓓ','ᓕ','ᓗ','ᓚ'},
+            new char[] {'ᕃ','ᕆ','ᕈ','ᕋ' }
+        };
+
+        //ᕃᕆᕈᕋ
+        List<Button> sylbtn;
+        public Syllabics()
+        {
+            InitializeComponent();
+        }
+        void sylclick(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            txtSyl.Text += btn.Text;
+            if (txtRoman.Text.Count() > 0)
+                txtRoman.Text += "-";
+            txtRoman.Text += btn.Name; 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablegroup.ColumnCount = syl[0].Count();
+            tablegroup.RowCount = syl.Count();
+            sylbtn = new List<Button>();
+            for(int l = 0; l < syl.Count(); l++)
+            {
+                for (int i = 0; i < syl[l].Count(); i++)
+                {
+                    Button newbtn = new Button();
+
+                    //sylbtngroup.Controls.Add(newbtn);
+                    tablegroup.Controls.Add(newbtn, i, l);
+                    sylbtn.Add(newbtn);
+                    newbtn.Name = letters[l] + vow[i];
+                    newbtn.Text = syl[l][i].ToString();
+                    newbtn.TabIndex = 1;
+                    newbtn.Dock = DockStyle.Fill;
+                    newbtn.Font = new Font("Aboriginal", 15);
+                    newbtn.Click += sylclick;
+                    
+                    //newbtn.Size = new System.Drawing.Size(273, 196);
+                    //newbtn.Location = new System.Drawing.Point(156, 71);
+                }
+            }
+            ;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtRoman.Text = "";
+            txtSyl.Text = "";
+        }
+    }
+}
